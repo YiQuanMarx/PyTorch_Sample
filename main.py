26944d38e1092b7cc31928e12bd7331331d505d5
“YiQuanMarx” <bayuehaitang@163.com>
2023-10-06 13:57:55 +0800

    change main

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def main():
     optimizer=torch.optim.Adam(model.parameters(),lr=1e-3)
     # print(model)
 
-    for epoch in range(10):
+    for epoch in range(4):
         model.train()
         # 加载保存的模型参数
         for batch_idx, (x, label) in enumerate(cifar_train):
@@ -50,6 +50,7 @@ def main():
 
         
         # test
+        # Tip 6: model
         model.eval()
         with torch.no_grad():
             total_correct=0
@@ -74,3 +75,33 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+'''
+Tip 6: 保存模型参数
+
+在你提供的代码中，模型参数存储在`model`对象中。这里是一些说明：
+
+1. `model` 是一个神经网络模型，它包含了各种层和参数。
+
+2. `model.train()` 和 `model.eval()` 分别将模型设置为训练模式和评估模式。在训练模式下，模型会计算梯度以进行反向传播和参数更新。在评估模式下，模型不会计算梯度，这通常用于在测试集上进行性能评估。
+
+3. 训练过程中，通过反向传播算法，模型的参数会被更新以最小化损失函数。
+
+4. 你可以使用 `model.state_dict()` 将模型的参数以字典的形式获取到。这个字典包含了模型的所有可训练参数。
+
+如果你想要将模型的参数保存到文件中，你可以使用以下代码：
+
+```python
+torch.save(model.state_dict(), 'model.pth')
+```
+
+这会将模型的参数保存到名为`model.pth`的文件中。如果需要重新加载模型，可以使用以下代码：
+
+```python
+model.load_state_dict(torch.load('model.pth'))
+```
+
+这会将之前保存的参数加载到模型中。
+
+请注意，模型的参数是在训练过程中进行更新的，所以保存的参数将是训练后的状态。如果你想要保存中间训练状态的参数，可以在适当的时候进行保存。
+'''
