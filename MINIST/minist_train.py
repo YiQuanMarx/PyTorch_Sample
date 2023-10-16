ce76b0705af8adcd46d6d69b52a7870788eea388
YiQuanMarx <bayuehaitang@163.com>
2023-10-16 23:09:15 +0800

    easy

--- MINIST/minist_train.py
+++ MINIST/minist_train.py
@@ -9,4 +9,24 @@ from matplotlib import pyplot as plt
 from utils import plot_image,plot_curve,one_hot
 
 # load dataset
+train_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST('mnist_data',train=True,download=True,
+                               transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor(),
+                                   torchvision.transforms.Normalize((0.1307,),(0.3081,))
+                               ])),
+    batch_size=200,shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST('mnist_data/',train=False,download=True,
+                                 transform=torchvision.transforms.Compose([
+                                      torchvision.transforms.ToTensor(),
+                                      torchvision.transforms.Normalize((0.1307,),(0.3081,))
+                                 ])),
+    batch_size=200,shuffle=False)
+
+x,y = next(iter(train_loader))
+print(x.shape,y.shape,x.min(),x.max())
+
+
 
